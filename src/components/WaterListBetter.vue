faf36a89f6efe32dd4f7304f86d77f5fd7d5cef0
黄景圣 <1141752341@qq.com>
2024-03-16 22:25:16 +0800

    代码整理

--- src/components/WaterListBetter.vue
+++ src/components/WaterListBetter.vue
@@ -6,17 +6,27 @@
     <a class="link" href="#/">基础版</a>
   </div>
   <TransitionGroup :name="page.move ? 'group' : ''" tag="div" class="water-list flex" ref="wrapEl">
-    <TransitionGroup :name="page.move ? 'group' : ''" tag="div" class="water-list-column" v-for="column in page.columnList" :key="'col-' + column.index" :data-height="column.height">
+    <TransitionGroup
+      v-for="column in page.columnList"
+      :name="page.move ? 'group' : ''"
+      tag="div"
+      class="water-list-column"
+      :key="'col-' + column.index"
+      :data-height="column.height"
+      :data-index="column.index"
+    >
       <div class="water-list-item" v-for="item in column.list" :key="item.id">
         <img class="pic" :src="item.photo" alt="">
         <div class="title">{{ item.title }}</div>
         <div class="content ellipsis_2">{{ item.text }}</div>
+        <!-- TODO: 调试用 -->
+        <!-- <div class="content ellipsis_2">当前节点的所在列的高度 - {{ item.currentColumnHeight }}</div> -->
       </div>
     </TransitionGroup>
   </TransitionGroup>
 </template>
 <script lang="ts" setup>
-import { ref, reactive, computed, onMounted, onUnmounted } from "vue";
+import { reactive, onMounted, onUnmounted } from "vue";
 import { useRequest, type ItemList, type ItemInfo } from "./hooks";
 
 const { getList, defaultPic } = useRequest();
@@ -37,71 +47,26 @@ let cacheList: ItemList = [];
  */
 function setColumnList(addList: ItemList, reset = false) {
   let result = JSON.parse(JSON.stringify(page.columnList)) as typeof page.columnList;
-  /** 设置列的索引 */
-  let columnIndex = -1; 
   if (reset) {
     result = new Array(page.column).fill(0).map((_, index) => ({ index: index, list: [], height: 0 }));
-    columnIndex = 0;
     cacheList = [];
   }
   addList.forEach(item => {
-    if (columnIndex < 0) {
-      // 从这里开始，将以最低高度列的数组进行添加数据，这样就不会出现某一列高度与其他差距较大的情况
-      result.sort((a, b) => a.height - b.height);
-      // console.log("数据添加前 >>", item.id, result.map(ele => ({ index: ele.index, height: ele.height })));
-      result[0].list.push(item);
-      result[0].height += item.height!;
-      // console.log("数据添加后 >>", item.id, result.map(ele => ({ index: ele.index, height: ele.height })));
-      // console.log("--------------------");
-      console.log("执行");
-    } else {
-      result[columnIndex].list.push(item);
-      result[columnIndex].height += item.height!;
-      columnIndex++;
-      if (columnIndex >= page.column) {
-        columnIndex = -1;
-      }
-      console.log("填满");
-    }
+    result.sort((a, b) => a.height - b.height);
+    // console.log("数据添加前 >>", item.id, result.map(ele => ({ index: ele.index, height: ele.height })));
+    const target = result[0]; 
+    target.height += item.height!;
+    item.currentColumnHeight = target.height; // TODO: 设置一个调试值
+    target.list.push(item);
+    // console.log("数据添加后 >>", item.id, result.map(ele => ({ index: ele.index, height: ele.height })));
+    // console.log("--------------------");
+
   });
   cacheList = cacheList.concat(addList);
   // 最后排一下原来的顺序再返回即可
   result.sort((a, b) => a.index - b.index);
   page.columnList = result;
 }
-
-// const pageList = computed(function() {
-//   const result = new Array(page.column).fill(0).map((_, index) => ({ id: index, list: [] as ItemList, height: 0 }));
-//   /** 设置列的索引 */
-//   let columnIndex = 0;
-//   // TODO: 这里放一个引用值，用于手动更新；
-//   page.update;
-//   // 开始组装数据
-//   for (let i = 0; i < cacheList.length; i++) {
-//     const item = cacheList[i];
-//     if (columnIndex < 0) {
-//       // 从这里开始，将以最低高度列的数组进行添加数据，这样就不会出现某一列高度与其他差距较大的情况
-//       result.sort((a, b) => a.height - b.height);
-//       // console.log("数据添加前 >>", item.id, result.map(ele => ({ index: ele.id, height: ele.height })));
-//       result[0].list.push(item);
-//       result[0].height += item.height!;
-//       // console.log("数据添加后 >>", item.id, result.map(ele => ({ index: ele.id, height: ele.height })));
-//       // console.log("--------------------");
-//     } else {
-//       result[columnIndex].list.push(item);
-//       result[columnIndex].height += item.height!;
-//       columnIndex++;
-//       if (columnIndex >= page.column) {
-//         columnIndex = -1;
-//       }
-//     }
-//   }
-//   console.log("重新计算列表 !!----------!!");
-//   // 最后排一下原来的顺序再返回即可
-//   result.sort((a, b) => a.id - b.id);
-//   // console.log("处理过的数据列表 >>", result);
-//   return result;
-// });
 
 /**
  * 加载所有图片并设置对应的宽高
